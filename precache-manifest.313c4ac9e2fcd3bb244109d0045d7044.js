6235bb37d06383290cd41fcb55d7cd07701c8db2
Code Sandbox <hello@codesandbox.io>
2023-06-25 14:49:08 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.313c4ac9e2fcd3bb244109d0045d7044.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c196fdf0ac99d6c4238a80748bcb2bd",
+    "url": "https://.github.io/csb-smfssw/index.html"
+  },
+  {
+    "revision": "d99436fa87760e95b839",
+    "url": "https://.github.io/csb-smfssw/static/css/main.9bc6d313.chunk.css"
+  },
+  {
+    "revision": "f3211b4578900ff138ff",
+    "url": "https://.github.io/csb-smfssw/static/js/0.e4643634.chunk.js"
+  },
+  {
+    "revision": "bc92bffb6d6be61e2f21",
+    "url": "https://.github.io/csb-smfssw/static/js/10.42a46304.chunk.js"
+  },
+  {
+    "revision": "ebc6f349370c7c641615",
+    "url": "https://.github.io/csb-smfssw/static/js/11.e7014088.chunk.js"
+  },
+  {
+    "revision": "910645590ff70fc614d4",
+    "url": "https://.github.io/csb-smfssw/static/js/3.7fcf0de4.chunk.js"
+  },
+  {
+    "revision": "15723851b11509ef1b9a8dbd2a0ef589",
+    "url": "https://.github.io/csb-smfssw/static/js/3.7fcf0de4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a63b3216519790253aa",
+    "url": "https://.github.io/csb-smfssw/static/js/4.f9af6604.chunk.js"
+  },
+  {
+    "revision": "fcd74bbdc7ddcba95290",
+    "url": "https://.github.io/csb-smfssw/static/js/5.76a26bca.chunk.js"
+  },
+  {
+    "revision": "374658371e5d9c95e00ea15067d24dad",
+    "url": "https://.github.io/csb-smfssw/static/js/5.76a26bca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "404e000296f54e1b4367",
+    "url": "https://.github.io/csb-smfssw/static/js/6.fd7a8dd9.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "https://.github.io/csb-smfssw/static/js/6.fd7a8dd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d455c604699ec47e603",
+    "url": "https://.github.io/csb-smfssw/static/js/7.b1331a1d.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "https://.github.io/csb-smfssw/static/js/7.b1331a1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "169b9aa8e7cba9c180aa",
+    "url": "https://.github.io/csb-smfssw/static/js/8.af17591c.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "https://.github.io/csb-smfssw/static/js/8.af17591c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd3266dff49873cc4b19",
+    "url": "https://.github.io/csb-smfssw/static/js/9.6e5bc6a4.chunk.js"
+  },
+  {
+    "revision": "d99436fa87760e95b839",
+    "url": "https://.github.io/csb-smfssw/static/js/main.9d40b5ef.chunk.js"
+  },
+  {
+    "revision": "dcbf68a886329a83c807",
+    "url": "https://.github.io/csb-smfssw/static/js/runtime-main.080a1dfa.js"
+  },
+  {
+    "revision": "ec78e8e86a08b2278fa9baf09388d697",
+    "url": "https://.github.io/csb-smfssw/static/media/chef.ec78e8e8.png"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "https://.github.io/csb-smfssw/static/media/metamask-fox.c06f3a3e.svg"
+  },
+  {
+    "revision": "37f2bc6ecee2eb29b3cf6799ce58bf58",
+    "url": "https://.github.io/csb-smfssw/static/media/wallet-connect.37f2bc6e.svg"
+  }
+]);
